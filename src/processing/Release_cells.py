729052cebdebcdfd6db8c207215e9cbf7a4a358c
dmanral <darshika.manral@gmail.com>
2023-02-14 11:00:56 +0100

    Updated map of release cells and stranding locations

--- src/processing/Release_cells.py
+++ src/processing/Release_cells.py
@@ -5,6 +5,8 @@ import matplotlib.colors as clr
 import pandas as pd
 from matplotlib.lines import Line2D
 from copy import copy
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
 
 # find nearest coastal cell to defined beaching location, to release in water,
@@ -50,10 +52,22 @@ color_land = copy(plt.get_cmap('Reds'))(0)
 color_ocean = copy(plt.get_cmap('Reds'))(128)
 
 fig = plt.figure(figsize=(16, 10), dpi=300)
-ax = plt.axes()
+ax = plt.axes(projection=ccrs.PlateCarree())
+gl = ax.gridlines(draw_labels=True)
+gl.xlines = False
+gl.ylines = False
+gl.top_labels = False
+gl.right_labels = False
+gl.xlabel_style = {'size': 14, 'color': 'k'}
+gl.ylabel_style = {'size': 14, 'color': 'k'}
 colormap = clr.ListedColormap(['skyblue', 'white'])
 plt.title('Released particles for each stranding location', fontsize=20, pad=20)
-ax.pcolormesh(fieldMesh_x[150:251, 200:226], fieldMesh_y[150:251, 200:226], lmask[150:250, 200:225], cmap=colormap)
+# ax.coastlines(resolution='50m')
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.OCEAN)
+ax.add_feature(cfeature.BORDERS, edgecolor='gray')
+
+# ax.pcolormesh(fieldMesh_x[150:251, 200:226], fieldMesh_y[150:251, 200:226], lmask[150:250, 200:225], cmap=colormap)
 # ax.scatter(fieldMesh_x, fieldMesh_y, c=landMask, s=20,
 #            cmap='Reds_r', vmin=-0.05, vmax=0.05, edgecolors='k')
 
@@ -76,11 +90,13 @@ for index, st in stations.iterrows():
 
     plt.scatter(fieldMesh_x_re, fieldMesh_y_re, c='r', alpha=0.5, s=0.5)
     plt.scatter(strand_lon, strand_lat, c='black', marker='x', s=200)
-    ax.text(strand_lon, strand_lat - 0.3, st['Location'],
+    ax.text(strand_lon + 0.1, strand_lat - 0.05, st['Location'],
             bbox=dict(facecolor='white', alpha=0.7, pad=0.2, edgecolor='none'),
             fontsize=15)
 ax.tick_params(axis='both', labelsize=13)
-ax.set_xlim(3.2, 5)
+# ax.set_xlim(3.2, 5)
+# ax.set_ylim(50, 55)
+ax.set_xlim(1, 6)
 ax.set_ylim(50, 55)
 
 custom_lines = [Line2D([], [], color='black', marker='x', linestyle='None', markersize=12, label='Stranding location'),
@@ -93,7 +109,7 @@ ax.legend(handles=custom_lines, bbox_to_anchor=(.01, .93), loc='center left', bo
 # ax.legend(custom_lines, ['ocean point', 'land point'], bbox_to_anchor=(.01, .93), loc='center left', borderaxespad=0.,
 #           framealpha=1)
 
-plt.savefig(home_dir + 'Plots/ReleaseCells.jpeg',
+plt.savefig(home_dir + 'Plots/MapReleaseCells.jpeg',
             bbox_inches='tight',
             pad_inches=0.2)
 # plt.show()
