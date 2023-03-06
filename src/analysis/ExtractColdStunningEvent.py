d10265d7173beac61b8f4969e7df972b67785c27
Darshika Manral <darshika.manral@gmail.com>
2023-03-06 16:30:55 +0100

    Updated the code for new figures (Version 2 draft)

--- src/analysis/ExtractColdStunningEvent.py
+++ src/analysis/ExtractColdStunningEvent.py
@@ -13,8 +13,17 @@ import pandas as pd
 import util
 import csv
 from matplotlib.lines import Line2D
+import cartopy.feature as cfeature
+import sys
 
-home_folder = '/Users/dmanral/Desktop/Analysis/Ridley/'
+args = sys.argv
+assert len(args) == 2
+
+wind = args[1]
+print(wind)
+
+home_folder = '/nethome/manra003/analysis/KempRidley/'
+data_folder = '/nethome/manra003/KempsRidley_turtle_strandings/data/'
 
 threshold_t10 = 10
 threshold_t12 = 12
@@ -24,7 +33,15 @@ n_particles = 10000
 days = 120
 
 figure_dpi = 300
-stations = pd.read_csv(home_folder + 'Locations_NL.csv', index_col=1)
+stations = pd.read_csv(data_folder + 'Locations_NL.csv')
+
+ # we need to coordinates file to access the corner points - glamf/gphif
+true_lmask = np.genfromtxt(data_folder + 'true_landMask_296x_374y', delimiter=None)
+U_ds = xr.open_dataset('/storage/shared/oceanparcels/input_data/CMEMS/NWSHELF_MULTIYEAR_PHY_004_009/metoffice_foam1_amm7_NWS_CUR_dm20141211.nc')
+lons = U_ds.longitude
+lats = U_ds.latitude
+fieldMesh_x, fieldMesh_y = np.meshgrid(lons, lats)
+
 # summary file fields
 with open(home_folder + 'summary_file.csv', 'w', newline='') as file:
     writer = csv.writer(file)
@@ -35,11 +52,11 @@ with open(home_folder + 'summary_file.csv', 'w', newline='') as file:
 # 'Westerschouwen-Schouwen', 'Monster', 'DenHelder', 'Westkapelle', 'IJmuiden'
 for ind in stations.index:
     s = stations['Location'][ind]
-    # s = 'Westerschouwen-Schouwen'
-
-    file = 'Sum_BK_NoWind_curr+stokes_120days_{0}'.format(s)
-    data_ds = xr.open_dataset(home_folder + 'Simulations/{0}.nc'.format(file))
-
+    if wind == '0pWind':
+        data_ds = xr.open_dataset(home_folder + 'simulations/{0}/Sum_BK_{0}_curr+stokes_120days_{1}.nc'.format(wind, s))
+    else:
+        data_ds = xr.open_dataset(home_folder + 'simulations/{0}/Sum_BK_{0}_curr+stokes+wind_120days_{1}.nc'.format(wind, s))
+        
     # remove all zero temperature and beached particles (only at sea, beached=0)
     ds = data_ds.where((data_ds['theta'] != 0.) & (data_ds['beached'] == 0))
     mean_theta = ds.theta[:, 1:].mean(dim='traj', skipna=True).values
@@ -100,16 +117,16 @@ for ind in stations.index:
     print('mean distance 12 C: ', np.nanmean(dis_12t))
     print('mean distance 14 C: ', np.nanmean(dis_14t))
 
-    with open(home_folder + 'summary_file.csv', 'a', newline='') as f:
+    with open(home_folder + 'outputs/summary_file_{0}.csv'.format(wind), 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow([s, min(T10_t), max(T10_t), np.around(np.nanmean(dis_10t), 1),
                          min(T12_t), max(T12_t), np.around(np.nanmean(dis_12t), 1),
                          min(T14_t), max(T14_t), np.around(np.nanmean(dis_14t), 1)])
 
-    custom_size = 15
+    custom_size = 18
     fig, ax = plt.subplots(ncols=2, nrows=1,
                            dpi=figure_dpi, figsize=(16, 8))
-    fig.suptitle(file)
+    fig.suptitle('Time_{0}_{1}_log'.format(wind, s))
     ax[0].axhline(threshold_t10, c='b', linestyle='--', label='10°C')
     ax[0].axhline(threshold_t12, c='orange', linestyle='--', label='12°C')
     ax[0].axhline(threshold_t14, c='red', linestyle='--', label='14°C')
@@ -149,22 +166,13 @@ for ind in stations.index:
     # for i in range(10000):
     #     plt.plot(ds.time[i, 1:], ds.theta[i, 1:], c='royalblue')
     plt.subplots_adjust(wspace=0.3)
-    plt.savefig(home_folder + 'Simulations/Plots/{0}_log.jpeg'.format(file),
+    plt.savefig(home_folder + 'outputs/{0}/Time_{0}_{1}_log.jpeg'.format(wind, s),
                 bbox_inches='tight',
                 pad_inches=0.2)
     # plt.show()
     print("First plot completed")
 
-    # we need to coordinates file to access the corner points - glamf/gphif
-    model_mask_file = home_folder + 'data/landMask_297x_375y'
-    landMask = np.genfromtxt(model_mask_file, delimiter=None)
-    true_lmask = np.genfromtxt(home_folder + 'data/true_landMask_296x_374y', delimiter=None)
-    U_ds = xr.open_dataset(home_folder + 'data/metoffice_foam1_amm7_NWS_CUR_dm20141211.nc')
-    lons = U_ds.longitude
-    lats = U_ds.latitude
-    fieldMesh_x, fieldMesh_y = np.meshgrid(lons, lats)
-
-    custom_size = 11
+    custom_size = 15
 
     figure2 = plt.figure(figsize=(12, 8), dpi=figure_dpi)
     ax2 = plt.axes(projection=ccrs.PlateCarree())
@@ -175,13 +183,21 @@ for ind in stations.index:
     gl.right_labels = False
     gl.xlabel_style = {'size': custom_size, 'color': 'k'}
     gl.ylabel_style = {'size': custom_size, 'color': 'k'}
-    # ax2.coastlines(resolution='50m')
+    ax2.add_feature(cfeature.LAND)
+
+    # ax.coastlines(resolution='50m')
+    # ax2.set_xlim(min_lon, max_lon)
+    # ax2.set_ylim(min_lat, max_lat)
     ax2.set_title('Locations where temperature crosses threshold temperatures: {0}'.format(s))
-    colormap = colors.ListedColormap(['white', 'gainsboro'])
-    # ax2 = plt.axes()
-    ax2.pcolormesh(fieldMesh_x[130:201, 135:231], fieldMesh_y[130:201, 135:231], true_lmask[130:200, 135:230],
+    colormap = colors.ListedColormap(['white', 'lightgrey'])
+    # ax2.pcolormesh(fieldMesh_x[130:201, 135:231], fieldMesh_y[130:201, 135:231], true_lmask[130:200, 135:230],
+    #                cmap=colormap)
+    ax2.pcolormesh(fieldMesh_x[130:221, 145:241], fieldMesh_y[130:221, 145:241], true_lmask[130:220, 145:240],
                    cmap=colormap)
 
+    ax2.add_feature(cfeature.COASTLINE, edgecolor='gray')
+    # ax2.add_feature(cfeature.BORDERS, edgecolor='lightgray')
+    # ax2.add_feature(cfeature.LAND, color='lightgray')
     ax2.scatter(lons_d10, lats_d10, c='b', s=4, label='10°C')
     ax2.scatter(lons_d14, lats_d14, c='tomato', s=4, label='14°C', alpha=0.7)
     ax2.scatter(lons_d12, lats_d12, c='orange', s=4, label='12°C', alpha=0.7)
@@ -195,7 +211,7 @@ for ind in stations.index:
         Line2D([], [], color='black', marker='x', linestyle='None', markersize=10, label='Stranding location')]
     ax2.legend(handles=custom_lines, loc='upper left', borderpad=0.8, prop={'size': custom_size})
 
-    plt.savefig(home_folder + 'Simulations/Plots/ColdStunningLocations_{0}.jpeg'.format(s),
+    plt.savefig(home_folder + 'outputs/{0}/Locations_{0}_{1}.jpeg'.format(wind, s),
                 bbox_inches='tight',
                 pad_inches=0.2)
     # plt.show()
--- src/visualizations/SimulationAnimation.py
+++ src/visualizations/SimulationAnimation.py
@@ -5,85 +5,93 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 from matplotlib import colors, colorbar
 import cmocean
+import pandas as pd
+import sys
 
-wind = '1pWind'
-base_folder = '/Users/dmanral/Desktop/Analysis/Ridley/'
-home_folder = '/Users/dmanral/Desktop/Analysis/Ridley/Sim{0}/'.format(wind)
+args = sys.argv
+assert len(args) == 2
+
+wind = args[1]
+print(wind)
+
+base_folder = '/nethome/manra003/analysis/KempRidley/'
 
 # we need to coordinates file to access the corner points - glamf/gphif
-model_mask_file = base_folder + 'data/landMask_297x_375y'
-
-landMask = np.genfromtxt(model_mask_file, delimiter=None) \
- \
-# 'WesterschouwenSchouwen', 'Monster', 'DenHelder', 'Westkapelle', 'IJmuiden'
-s = 'Westkapelle'
-
-# file = 'Sum_BK_NoWind_curr+stokes_120days_{0}'.format(s)
-# ds = xr.open_dataset(home_folder + 'Simulations/{0}.nc'.format(file))
-if wind == '0pWind':
-    file = 'Sum_BK_{0}_curr+stokes_120days_{1}'.format(wind, s)
-    ds = xr.open_dataset(home_folder + '{0}.nc'.format(file))
-else:
-    file = 'Sum_BK_{0}_curr+stokes+wind_120days_{1}'.format(wind, s)
-    ds = xr.open_dataset(home_folder + 'Sum_BK_{0}_curr+stokes+wind_120days_{1}.nc'.format(wind, s))
-
-print(ds)
-
-U_ds = xr.open_dataset(base_folder + 'data/metoffice_foam1_amm7_NWS_CUR_dm20141211.nc')
+U_ds = xr.open_dataset('/storage/shared/oceanparcels/input_data/CMEMS/NWSHELF_MULTIYEAR_PHY_004_009/metoffice_foam1_amm7_NWS_CUR_dm20141211.nc')
 lons = U_ds.longitude
 lats = U_ds.latitude
 fieldMesh_x, fieldMesh_y = np.meshgrid(lons, lats)
-true_lmask = np.genfromtxt(base_folder + 'data/true_landMask_296x_374y', delimiter=None)
-fig, [ax, cax] = plt.subplots(1, 2, gridspec_kw={"width_ratios": [50, 1]}, dpi=300)
-colormap = colors.ListedColormap(['white', 'gainsboro'])
+true_lmask = np.genfromtxt('/nethome/manra003/KempsRidley_turtle_strandings/data/true_landMask_296x_374y', delimiter=None)
 
-# ax.pcolormesh(fieldMesh_x, fieldMesh_y, landMask, cmap=colormap,
-#               shading='auto')
-ax.pcolormesh(fieldMesh_x[100:251, 100:226], fieldMesh_y[100:251, 100:226], true_lmask[100:250, 100:225], cmap=colormap)
-# ax.pcolormesh(fieldMesh_x[150:251,175:226], fieldMesh_y[150:251,175:226], landMask[150:250,175:225], cmap=colormap)
+stations = pd.read_csv('/nethome/manra003/KempsRidley_turtle_strandings/data/Locations_NL.csv')
 
+for index, station in stations.iterrows():
+    
+    s = station['Location']
+    if wind == '0pWind':
+        ds = xr.open_dataset(base_folder + 'simulations/{0}/Sum_BK_{0}_curr+stokes_120days_{1}.nc'.format(wind, s))
+    else:
+        ds = xr.open_dataset(base_folder + 'simulations/{0}/Sum_BK_{0}_curr+stokes+wind_120days_{1}.nc'.format(wind, s))
 
-# region Animation
-output_dt = timedelta(days=1)
-time_range = np.arange(np.nanmax(ds['time'].values),
-                       np.nanmin(ds['time'].values) - np.timedelta64(output_dt),
-                       -output_dt)
-print('Time_range: ', len(time_range))
+    print(ds)
 
-# release locations
-time_id = np.where(ds['time'] == time_range[0])
+    fig, [ax, cax] = plt.subplots(1, 2, gridspec_kw={"width_ratios": [50, 1]}, dpi=300)
+    colormap = colors.ListedColormap(['white', 'gainsboro'])
 
-theta1 = ds['theta']
-
-# temp_cmp = cmocean.cm.balance
-temp_cmp = plt.cm.coolwarm
-norm = colors.Normalize(vmin=5, vmax=20)
-
-cb1 = colorbar.ColorbarBase(cax, cmap=temp_cmp,
-                            norm=norm,
-                            orientation='vertical', label='Temperature (°C)')
-scatter = ax.scatter(ds['lon'].values[time_id], ds['lat'].values[time_id], s=1)
-# d=270
-# ax.scatter(ds.lon[:, d], ds.lat[:, d], c=ds.theta[:, d], cmap=temp_cmp, s=0.5)
-# plt.show()
-
-t = np.datetime_as_string(time_range[0], unit='m')
-title = ax.set_title('Particles at z = 0 m and time = ' + t)
+    # ax.pcolormesh(fieldMesh_x, fieldMesh_y, landMask, cmap=colormap,
+    #               shading='auto')
+    ax.pcolormesh(fieldMesh_x[100:221, 145:241], fieldMesh_y[100:221, 145:241], true_lmask[100:220, 145:240],
+                   cmap=colormap)
+    
+    # ax.pcolormesh(fieldMesh_x[150:251,175:226], fieldMesh_y[150:251,175:226], landMask[150:250,175:225], cmap=colormap)
 
 
-def animate(i):
-    t = np.datetime_as_string(time_range[i], unit='m')
+    # region Animation
+    output_dt = timedelta(days=1)
+    time_range = np.arange(np.nanmax(ds['time'].values),
+                           np.nanmin(ds['time'].values) - np.timedelta64(output_dt),
+                           -output_dt)
+    print('Time_range: ', len(time_range))
 
-    time_id = np.where(ds['time'] == time_range[i])
-    title.set_text('Particles at z = 0 m and time = ' + t)
+    # release locations
+    time_id = np.where(ds['time'] == time_range[0])
 
-    scatter.set_offsets(np.c_[ds['lon'].values[time_id], ds['lat'].values[time_id]])
-    scatter.set_color(temp_cmp(norm(theta1.values[time_id])))
+    theta1 = ds['theta']
+
+    # temp_cmp = cmocean.cm.balance
+    temp_cmp = plt.cm.coolwarm
+    norm = colors.Normalize(vmin=5, vmax=20)
+
+    cb1 = colorbar.ColorbarBase(cax, cmap=temp_cmp,
+                                norm=norm,
+                                orientation='vertical', label='Temperature (°C)')
+    scatter = ax.scatter(ds['lon'].values[time_id], ds['lat'].values[time_id], s=1)
+    strand_lon, strand_lat = station['Longitude'], station['Latitude']
+    plt.scatter(strand_lon, strand_lat, c='black', marker='x', s=200)
+    ax.text(strand_lon + 0.3, strand_lat - 0.1, s,
+            bbox=dict(facecolor='white', alpha=0.7, pad=0.2, edgecolor='none'),
+            fontsize=8)
+    # d=270
+    # ax.scatter(ds.lon[:, d], ds.lat[:, d], c=ds.theta[:, d], cmap=temp_cmp, s=0.5)
+    # plt.show()
+
+    t = np.datetime_as_string(time_range[0], unit='m')
+    title = ax.set_title('Particles at time = ' + t)
 
 
-size = len(time_range)
-anim = FuncAnimation(fig, animate, frames=size, interval=200)
-anim.save(home_folder + '{0}.mp4'.format(file))
-# endregion
+    def animate(i):
+        t = np.datetime_as_string(time_range[i], unit='m')
 
-print('animation saved')
+        time_id = np.where(ds['time'] == time_range[i])
+        title.set_text(s + ': Backward simulation at time = ' + t)
+
+        scatter.set_offsets(np.c_[ds['lon'].values[time_id], ds['lat'].values[time_id]])
+        scatter.set_color(temp_cmp(norm(theta1.values[time_id])))
+
+
+    size = len(time_range)
+    anim = FuncAnimation(fig, animate, frames=size, interval=200)
+    anim.save(base_folder + 'outputs/{0}/{0}_{1}.mp4'.format(wind, s))
+    # endregion
+
+    print('animation saved')
