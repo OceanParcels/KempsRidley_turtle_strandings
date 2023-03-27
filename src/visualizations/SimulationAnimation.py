c277f0b0accfc9c9e9b4589780967affa56c9f60
Darshika Manral <darshika.manral@gmail.com>
2023-03-27 13:45:10 +0200

    correcting colorbar axis display

--- src/visualizations/SimulationAnimation.py
+++ src/visualizations/SimulationAnimation.py
@@ -4,18 +4,19 @@ from datetime import timedelta
 import numpy as np
 from matplotlib.animation import FuncAnimation
 from matplotlib import colors, colorbar
+from matplotlib.transforms import Bbox
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import pandas as pd
 import sys
 
-args = sys.argv
-assert len(args) == 2
+# args = sys.argv
+# assert len(args) == 2
 
-wind = args[1]
+wind = '0pWind' #args[1]
 print(wind)
-min_lon, max_lon = -15, 8
-min_lat, max_lat = 42, 57
+min_lon, max_lon = -15, 6
+min_lat, max_lat = 45, 55
 
 base_folder = '/nethome/manra003/analysis/KempRidley/'
 
@@ -24,7 +25,7 @@ U_ds = xr.open_dataset('/storage/shared/oceanparcels/input_data/CMEMS/NWSHELF_MU
 lons = U_ds.longitude
 lats = U_ds.latitude
 fieldMesh_x, fieldMesh_y = np.meshgrid(lons, lats)
-lmask = np.genfromtxt('/nethome/manra003/KempsRidley_turtle_strandings/data/landMask_297x_375y', delimiter=None)
+true_lmask = np.genfromtxt('/nethome/manra003/KempsRidley_turtle_strandings/data/true_landMask_296x_374y', delimiter=None)
 
 stations = pd.read_csv('/nethome/manra003/KempsRidley_turtle_strandings/data/Locations_NL.csv')
 
@@ -36,7 +37,11 @@ for index, station in stations.iterrows():
     else:
         ds = xr.open_zarr(base_folder + 'simulations/{0}/Sum_BK_{0}_curr+stokes+wind_120days_{1}.zarr'.format(wind, s))
 
-    fig, [ax, cax] = plt.subplots(1, 2, gridspec_kw={"width_ratios": [50, 1]}, subplot_kw={'projection': ccrs.PlateCarree()}, dpi=300)
+    fig = plt.figure(figsize=(10,5), dpi=300)
+    gs = fig.add_gridspec(nrows=1, ncols=2, width_ratios=[60, 2])
+
+    # Add the left subplot
+    ax = fig.add_subplot(gs[0], projection=ccrs.PlateCarree())
     colormap = colors.ListedColormap(['white', 'gainsboro'])
     gl = ax.gridlines(draw_labels=True)
     gl.xlines = False
@@ -49,11 +54,12 @@ for index, station in stations.iterrows():
     ax.add_feature(cfeature.COASTLINE, edgecolor='gray')
     ax.set_xlim(min_lon, max_lon)
     ax.set_ylim(min_lat, max_lat)
-    ax.pcolormesh(fieldMesh_x, fieldMesh_y, lmask, cmap=colormap)
-    # ax.pcolormesh(fieldMesh_x[100:221, 145:241], fieldMesh_y[100:221, 145:241], true_lmask[100:220, 145:240],
-    #                cmap=colormap)
-    ax.set_xlim(min_lon, max_lon)
-    ax.set_ylim(min_lat, max_lat)
+
+    latmin=0
+    latmax=375
+    lonmin=0
+    lonmax=297
+    ax.pcolormesh(fieldMesh_x[latmin:latmax+1,lonmin:lonmax+1], fieldMesh_y[latmin:latmax+1,lonmin:lonmax+1], true_lmask[latmin:latmax,lonmin:lonmax],cmap=colormap)
 
     # region Animation
     output_dt = timedelta(days=1)
@@ -67,21 +73,22 @@ for index, station in stations.iterrows():
 
     theta1 = ds['theta']
 
-    # temp_cmp = cmocean.cm.balance
+    # Add the right subplot for the colorbar
+    cax = fig.add_subplot(gs[1])
     temp_cmp = plt.cm.coolwarm
     norm = colors.Normalize(vmin=5, vmax=20)
-
     cb1 = colorbar.ColorbarBase(cax, cmap=temp_cmp,
                                 norm=norm,
                                 orientation='vertical', label='Temperature (°C)')
+
+    # Set the spacing between the subplots
+    plt.subplots_adjust(wspace=0.05)
     scatter = ax.scatter(ds['lon'].values[time_id], ds['lat'].values[time_id], s=1)
     strand_lon, strand_lat = station['Longitude'], station['Latitude']
-    plt.scatter(strand_lon, strand_lat, c='black', marker='x', s=200)
+    ax.scatter(strand_lon, strand_lat, c='black', marker='x', s=100)
     ax.text(strand_lon + 0.3, strand_lat - 0.1, s,
             bbox=dict(facecolor='white', alpha=0.7, pad=0.2, edgecolor='none'),
             fontsize=8)
-    # d=270
-    # ax.scatter(ds.lon[:, d], ds.lat[:, d], c=ds.theta[:, d], cmap=temp_cmp, s=0.5)
     # plt.show()
 
     t = np.datetime_as_string(time_range[0], unit='m')
